cff0355f23c179e893d34940f0b6164fe3d968e4
Chad Ellison <chad.ellison0123@gmail.com>
2017-06-20 11:14:48 -0500

    add felt

--- src/App.js
+++ src/App.js
@@ -7,16 +7,24 @@ import LoginButton from './components/LoginButton'
 import SignUpButton from './components/SignUpButton'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      background: "felt"
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Texas Holdem</h2>
           <LoginButton />
           <SignUpButton />
         </div>
-        <BackgroundImage />
+        <BackgroundImage background={this.state.background} />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- src/components/BackgroundImage.js
+++ src/components/BackgroundImage.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import aces from '../images/aces.png'
+import felt from '../images/felt.png'
 import '../styles/backgroundImage.css'
 export default class BackgroundImage extends Component {
   render() {
+    let background;
+    if(this.props.background === "aces") {
+      background = aces
+    } else {
+      background = felt
+    }
+
     return(
       <div>
-        <img src={aces} alt='image of aces' className='background' />
+        <img src={background} alt='image of aces' className='background' />
       </div>
     )
   }
